6ec0c8c88c794f0ff713678fcdc481bf11ba3513
lfnandoo <devfernandoof@gmail.com>
2020-10-15 01:36:36 -0300

    feat: marker inside map

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -5,8 +5,16 @@ import "leaflet/dist/leaflet.css";
 
 import mapMarkerImg from "../../images/map-marker.svg";
 import { Link } from "react-router-dom";
-import { FiPlus } from "react-icons/fi";
-import { Map, TileLayer } from "react-leaflet";
+import { FiArrowRight, FiPlus } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -34,6 +42,15 @@ const OrphanagesMap: React.FC = () => {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        <Marker icon={mapIcon} position={[-22.7150256, -43.3777264]}>
+          <Popup closeButton={false} minWidth={240} maxWidth={248}>
+            Lar das menina
+            <Link to="/">
+              <FiArrowRight size={20} color="FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <Link to="">
--- src/pages/OrphanagesMap/styles.ts
+++ src/pages/OrphanagesMap/styles.ts
@@ -6,10 +6,6 @@ export const Container = styled.div`
 
   position: relative;
   display: flex;
-
-  .leaflet-container {
-    z-index: 5;
-  }
   
   > aside {
     width: 440px;
@@ -42,6 +38,43 @@ export const Container = styled.div`
     &:hover {
       background: #17d6eb;
     }
+  }
+
+  .leaflet-container {
+    z-index: 5;
+  }
+
+  .leaflet-popup-content-wrapper {
+    background: rgba(255, 255, 255, 0.8);
+    border-radius: 20px;
+    box-shadow: none;
+  }
+
+  .leaflet-popup-content {
+    color: #0089a5;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 8px 12px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .leaflet-popup-content-wrapper a {
+    width: 40px;
+    height: 40px;
+    background: #15c3d6;
+    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, .16);
+    border-radius: 12px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .leaflet-popup-tip-container {
+    display: none;
   }
 
 `;
