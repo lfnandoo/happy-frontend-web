5f9bac8c1882c8bd3ad13fc1f5c1e7a1d461ae9a
lfnandoo <devfernandoof@gmail.com>
2020-10-15 01:37:11 -0300

    feat: create orphanage page

---
+++ src/pages/CreateOrphanage/index.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import { useHistory } from "react-router-dom";
+
+import * as Style from "./styles";
+
+import { FiArrowLeft, FiPlus } from "react-icons/fi";
+
+import mapMarkerImg from "../../images/map-marker.svg";
+
+const happyMapIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60]
+});
+
+const CreateOrphanage: React.FC = () => {
+  const { goBack } = useHistory();
+
+  return (
+    <Style.Container>
+      <Style.Aside>
+        <img src={mapMarkerImg} alt="Happy" />
+
+        <footer>
+          <button type="button" onClick={goBack}>
+            <FiArrowLeft size={24} color="#FFF" />
+          </button>
+        </footer>
+      </Style.Aside>
+
+      <Style.MainContent>
+        <form>
+          <Style.Fieldset>
+            <Style.FieldName>Dados</Style.FieldName>
+
+            <Map
+              center={[-27.2092052, -49.6401092]}
+              style={{ width: "100%", height: 280 }}
+              zoom={15}
+            >
+              <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+              />
+
+              <Marker
+                interactive={false}
+                icon={happyMapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
+            </Map>
+
+            <Style.InputBlock>
+              <Style.InputLabel htmlFor="name">Nome</Style.InputLabel>
+              <input id="name" />
+            </Style.InputBlock>
+
+            <Style.InputBlock>
+              <Style.InputLabel htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </Style.InputLabel>
+              <textarea id="name" maxLength={300} />
+            </Style.InputBlock>
+
+            <Style.InputBlock>
+              <Style.InputLabel htmlFor="images">Fotos</Style.InputLabel>
+
+              <Style.UploadedImage></Style.UploadedImage>
+
+              <Style.NewImage>
+                <FiPlus size={24} color="#15b6d6" />
+              </Style.NewImage>
+            </Style.InputBlock>
+          </Style.Fieldset>
+
+          <Style.Fieldset>
+            <Style.FieldName>Visitação</Style.FieldName>
+
+            <Style.InputBlock>
+              <Style.InputLabel htmlFor="instructions">
+                Instruções
+              </Style.InputLabel>
+              <textarea id="instructions" />
+            </Style.InputBlock>
+
+            <Style.InputBlock>
+              <Style.InputLabel htmlFor="opening_hours">Nome</Style.InputLabel>
+              <input id="opening_hours" />
+            </Style.InputBlock>
+
+            <Style.InputBlock>
+              <Style.InputLabel htmlFor="open_on_weekends">
+                Atende fim de semana
+              </Style.InputLabel>
+
+              <Style.ButtonSelect isActive={true}>
+                <button type="button">Sim</button>
+                <button type="button">Não</button>
+              </Style.ButtonSelect>
+            </Style.InputBlock>
+          </Style.Fieldset>
+
+          <Style.ButtonConfirm type="submit">Confirmar</Style.ButtonConfirm>
+        </form>
+      </Style.MainContent>
+    </Style.Container>
+  );
+};
+
+export default CreateOrphanage;
---
+++ src/pages/CreateOrphanage/styles.ts
@@ -0,0 +1,198 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+
+  .leaflet-container {
+    margin-bottom: 40px;
+    border: 1px solid #d3d2d5;
+    border-radius: 20px;
+  }
+`;
+
+export const Aside = styled.aside`
+  position: fixed;
+  height: 100%;
+  padding: 32px 24px;
+  background: linear-gradient(329.54deg, #15B6D6 0%, #15D6D6 100%);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  > img {
+    width: 48px;
+  }
+
+  > a, button {
+    width: 48px;
+    height: 48px;
+
+    border: 0;
+
+    background: #12AFCB;
+    border-radius: 16px;
+
+    cursor: pointer;
+
+    transition: background-color 0.2s;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      background: #17D6EB;
+    }
+  }
+`;
+
+export const MainContent = styled.main`
+  flex: 1;
+
+  > form {
+    width: 700px;
+    margin: 64px auto;
+
+    background: #FFFFFF;
+    border: 1px solid #D3E2E5;
+    border-radius: 20px;
+
+    padding: 64px 80px;
+
+    overflow: hidden;
+  }
+`;
+
+export const Fieldset = styled.fieldset`
+  border: 0;
+
+  & + fieldset {
+    margin-top: 80px;
+  }
+`;
+
+export const FieldName = styled.legend`
+  width: 100%;
+
+  font-size: 32px;
+  line-height: 34px;
+  color: #5C8599;
+  font-weight: 700;
+
+  border-bottom: 1px solid #D3E2E5;
+  margin-bottom: 40px;
+  padding-bottom: 24px;
+`;
+
+export const InputBlock = styled.div`
+  & + div {
+    margin-top: 24px;
+  }
+
+  > input, textarea {
+    width: 100%;
+    background: #F5F8FA;
+    border: 1px solid #D3E2E5;
+    border-radius: 20px;
+    outline: none;
+    color: #5C8599;
+  }
+
+  > input {
+    height: 64px;
+    padding: 0 16px;
+  }
+
+  > textarea {
+    min-height: 120px;
+    max-height: 240px;
+    resize: vertical;
+    padding: 16px;
+    line-height: 28px;
+  }
+`;
+
+export const InputLabel = styled.label`
+  display: flex;
+  color: #8FA7B3;
+  margin-bottom: 8px;
+  line-height: 24px;
+
+  > span {
+    font-size: 14px;
+    color: #8FA7B3;
+    margin-left: 24px;
+    line-height: 24px;
+  }
+`;
+
+export const UploadedImage = styled.div``;
+
+export const NewImage = styled.label`
+  height: 96px;
+
+  background: #f5f8fa;
+  border: 1px dashed #96d2f0;
+  border-radius: 20px;
+  cursor: pointer;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonSelect = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  > button {
+    height: 64px;
+
+    background: #F5F8FA;
+    border: 1px solid #D3E2E5;
+    color: #5C8599;
+    cursor: pointer;
+    background: ${({ isActive }: { isActive: boolean }) =>  isActive ? "#EDFFF6" : ""};
+    border: ${({ isActive }: { isActive: boolean }) =>  isActive ? "1px solid #A1E9C5" : ""};
+    color: ${({ isActive }: { isActive: boolean }) =>  isActive ? "#37C77F" : ""};
+  }
+
+  > button:first-child {
+    border-radius: 20px 0px 0px 20px;
+  }
+
+  > button:last-child {
+    border-radius: 0 20px 20px 0;
+    border-left: 0;
+  }
+`;
+
+export const ButtonConfirm = styled.button`
+  margin-top: 64px;
+
+  width: 100%;
+  height: 64px;
+  border: 0;
+  cursor: pointer;
+  background: #3CDC8C;
+  border-radius: 20px;
+  color: #FFFFFF;
+  font-weight: 800;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: #36CF82;
+  }
+
+  > svg {
+    margin-right: 16px;
+  }
+`;
+
